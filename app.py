d572e6ff3c818efcf6f4f5e67074cdff7a92f4d8
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2022-03-11 00:47:07 +0000

    added erd

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 from application._init_ import app
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- application/models.py
+++ application/models.py
@@ -1,1 +1,20 @@
 from application._init_ import db
+
+class Account(db.Model):
+    pkuserid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    user_name = db.Column(db.String(50))
+    balance_gbp = db.Column(db.Float)
+    transaction = db.relationship('Transaction', backref='account')
+
+class Coin(db.Model):
+    pkcoinid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(5))
+    price = db.Column(db.Float)
+    transaction = db.relationship('Transaction', backref='coin')
+
+class Transaction(db.Model):
+    pktransactionid = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(4))
+    coinid = db.Column('coin_pkcoinid', db.Integer, db.ForeignKey('coin.pkcoinid'))
+    userid = db.Column('accoint_pkuserid', db.Integer, db.ForeignKey('account.pkuserid'))
