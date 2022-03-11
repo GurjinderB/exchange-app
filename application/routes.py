91dcdb6fc5903b2c6d6c18c9de9b3e45d83e5682
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2022-03-11 01:07:22 +0000

    classes added

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 from flask import redirect, url_for
 from application._init_ import app, db
-from application.models import Todo, Project
+from application.models import Account, Coin, Transaction
 from datetime import date, timedelta
+
