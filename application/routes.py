723c2e514a136bb4f4eaf6eb23a8a4783da06985
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2022-03-11 02:14:25 +0000

    html first commit

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,15 @@
-from flask import redirect, url_for
+from flask import redirect, render_template, url_for
 from application._init_ import app, db
 from application.models import Account, Coin, Transaction
 from datetime import date, timedelta
 
+@app.route('/')
+def home():
+    return f"{Account.query.count()} accounts: " + '<br>'.join(str(t) + " " + str(t.coin) for t in Account.query.all())
+
+@app.route('/create/<name>/<price>')
+def create(name, price):
+    coin = Coin(name=name, price=price)
+    db.session.add(coin)
+    db.session.commit()
+    return redirect(url_for('home'))
